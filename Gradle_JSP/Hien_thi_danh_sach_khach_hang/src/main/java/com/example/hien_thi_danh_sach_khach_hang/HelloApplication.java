d353a23d5c23b0a9e0ceef8bb7e1ae496c14c3a1
MoonismyRiver <keepermen1962gmail.com>
2022-07-22 16:23:49 +0700

    ok

---
+++ Gradle_JSP/Hien_thi_danh_sach_khach_hang/src/main/java/com/example/hien_thi_danh_sach_khach_hang/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.hien_thi_danh_sach_khach_hang;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
